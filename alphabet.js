7e1ac1ada31cb7c6d2c764127ae75094123b1333
Ben Vaughan <benjamin.vaughan@gmail.com>
2014-07-10 20:47:56 -0600

    Added jquery to alphabet.  Added responsive classes to basicPage.

--- alphabet.js
+++ alphabet.js
@@ -1,24 +1,47 @@
+jQuery(document).ready(function($) {
+    console.log($('#abcbox').val())
+    $('#button1').click(function() {
+        $('#abcbox').val('');
+        abc();
+    });
+    $('#button2').on('click', function() {
+        $('body').toggleClass('wow');
+    })
+});
+
 /**
  * Created by Ben Vaughan on 7/10/2014.
  */
 
 // split the alphabet, display every other letter.
 // Every third letter that is display needs to be capitalized.
-    
-var abc = 'abcdefghijklmnopqrstuvwxyz';
-var arr = abc.split('');
-var t = 1;
-for(i=0;i<arr.length;i++){
 
-    if(i%2 === 0){
-        if(t === 3) {
-            console.log(arr[i].toUpperCase());
-            document.write(arr[i].toUpperCase() + ", ");
-            t = 1;
-        } else {
-            console.log(arr[i]);
-            document.write(arr[i] + ", ");
-            t++;
+
+
+function abc() {
+    var abc = 'abcdefghijklmnopqrstuvwxyz';
+    var arr = abc.split('');
+    var t = 1;
+    var content ='';
+    for (i = 0; i < arr.length; i++) {
+
+
+        if (i % 2 === 0) {
+            if (t === 3) {
+                console.log(arr[i].toUpperCase());
+                // document.write(arr[i].toUpperCase() + ", ");
+                content = content + arr[i].toUpperCase() + ", ";
+                t = 1;
+            } else {
+                console.log(arr[i]);
+                // document.write(arr[i] + ", ");
+                content = content + arr[i] + ", ";
+                t++;
+            }
         }
     }
+
+    console.log(content);
+    $('#abcbox').val(content);
 }
+
